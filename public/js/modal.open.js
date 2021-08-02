543a474ce96d9aaa4c6d44345707f24d4b91cba2
Luan Chaves <lchavesdesousa@gmail.com>
2021-08-02 07:48:03 -0300

    Ajustes

--- public/js/modal.open.js
+++ public/js/modal.open.js
@@ -6,3 +6,13 @@ function openModal(){
 function closedModal(){
     document.getElementById('modalBuy').style.display = "none"
 }
+
+function finalized(){
+    document.getElementById('modalBuy').style.display = "none"
+    Swal.fire({
+        icon: 'success',
+        title: 'Eba, pedido realizado com sucesso!',
+        text: 'Está mensagem só é exibida ao estar logado e o form de compra preenchido(o que ainda não foi habilitado), obrigado por testar meu sistema, L. Chaves agradece (:',
+        footer: '<a href="./produtos.html"> Deseja tentar novamente?</a>'
+    })
+}
